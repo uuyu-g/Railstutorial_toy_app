3d7adaccfb8c33ceb0f30e17a22b7ab925cd3db2
yutaro <aqueous.mkli@gmail.com>
2019-02-18 10:24:51 +0900

    マイグレーション

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190217025830) do
+ActiveRecord::Schema.define(version: 20190218012318) do
+
+  create_table "microposts", force: :cascade do |t|
+    t.text "content"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
